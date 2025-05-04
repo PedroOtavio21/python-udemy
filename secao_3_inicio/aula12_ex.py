01fe9865a727f707f88c092015fb5facdf22a130
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2025-05-04 18:09:56 -0300

    Adiciona segundo exercício do módulo introdutório, contendo cálculo simples de IMC do usuário

---
+++ secao_3_inicio/aula12_ex.py
@@ -0,0 +1,19 @@
+# Aula 12 de Python
+
+# Exercício: Cálculo de IMC
+
+# Variáveis
+nome = 'Luiz Otávio'
+altura = 1.80
+peso = 95
+imc = peso / (altura ** 2)
+
+# Saída de código:
+print(nome, 'tem', altura, 'de altura', sep=' ')
+print('pesa', peso, 'quilos e seu IMC é', sep=' ')
+print(imc)
+"""
+Luiz Otávio tem 1.80 de altura,
+pesa 95 quilos e seu IMC é
+29.320987654320987
+"""
