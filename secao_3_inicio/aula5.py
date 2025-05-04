2322943ee2b7a1df4954c95779e07d7af8787749
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2025-05-03 23:11:42 -0300

    Adiciona aulas 5 e 6 do módulo introdutório, sobre tipo de dado booleano e coerção de dados em Python

---
+++ secao_3_inicio/aula5.py
@@ -0,0 +1,24 @@
+# Aula 5 de Python
+
+# Tipo de dado bool (Booleano)
+
+"""
+- Usado geralmente para casos de verdadeiro ou falso (condicionais, verificações e afins)
+- Possui apenas 2 valores: True e False
+"""
+print("Valores booleanos:")
+print(False)
+print(True)
+
+# Operadores booleanos
+
+"""
+- Há vários exemplos de operadores booleanos em linguagem de programação
+- Dentre eles, existe o operador "==", que serve para verificar se um elemento é igual a outro
+- Ex.: 5 == '5' (Não)
+"""
+print("Testes de operadores booleanos:")
+print(25 == 25)
+print(5 == 2)
+print(2 == "Teste")
+print(type(5==5)) # Retorna o tipo de dado
---
+++ secao_3_inicio/aula6.py
@@ -0,0 +1,20 @@
+# Aula 6 de Python
+
+# Conversão de tipos, coerção
+
+# type convertion, typecasting, coercion é o ato de converter um tipo em outro 
+print(1 + 5)
+print("Alex" + " " + "Sanches")
+# print("1" + 1) # Isto gera um erro em Python, já em JS não
+
+# Tipos imutáveis e primitivos
+
+"""
+- str
+- int
+- float
+- bool
+"""
+print(int('1'), type(int('1')))
+print(int('1') + 5)
+print(bool('')) # True
